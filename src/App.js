23f369d1089ffef877d4660facb0e6d72285ed06
J Perrin <perrinjack96@gmail.com>
2020-10-21 14:00:37 +0100

    Car Make Select box handles array of makes

--- src/App.js
+++ src/App.js
@@ -7,23 +7,41 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+  const [make, setMake] = React.useState('');
+  const databaseMakes = ['Audi', 'BMW', 'Tesla'];
+
+  const handleChange = (event) => {
+    setMake(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel id="demo-simple-select-outlined-label">Car Make</InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={make}
+          onChange={handleChange}
+          label="Car Make"
         >
-          Learn React
-        </a>
-      </header>
+          {databaseMakes.map((x) => (
+            <MenuItem value={x}>{x}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 }
