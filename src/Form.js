5cd6bba2882f6b1040a64e62b2f565a0b84b0a69
J Perrin <perrinjack96@gmail.com>
2020-10-21 20:15:44 +0100

    results route added

--- src/Form.js
+++ src/Form.js
@@ -1,114 +1,53 @@
 import React from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 160,
-    color: 'red',
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(6),
-  },
-}));
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { useHistory } from 'react-router-dom';
 
 function Form() {
-  const classes = useStyles();
-  const [make, setMake] = React.useState('');
-  const [model, setModel] = React.useState('');
-  const [modelList, setModelList] = React.useState([]);
-  const [engine, setEngine] = React.useState('');
-  const [engineDisabled, setEngineDisabled] = React.useState(true);
-  const [modelDisabled, setModelDisabled] = React.useState(true);
+  const history = useHistory();
+
+  const [place, setPlace] = React.useState('');
   const [buttonDisabled, setButtonDisabled] = React.useState(true);
-  const databaseMakes = ['Audi', 'BMW', 'Tesla'];
-  const databaseModels = {
-    Audi: ['TT', 'Q'],
-    BMW: ['2 series', '3 Series', '4 series', '1 Series'],
-    Tesla: ['Model X'],
-  };
-  const engineList = ['2.0'];
 
   const handleChangeMake = (event) => {
-    setMake(event.target.value);
-    setModelList(databaseModels[event.target.value]);
-    setModel('');
-    setEngine('');
-    setModelDisabled(false);
-    setEngineDisabled(true);
-    setButtonDisabled(true);
+    setPlace(event.target.value);
+    setButtonDisabled(!buttonDisabled);
   };
 
-  const handleChangeModel = (event) => {
-    setModel(event.target.value);
-    setEngine('');
-    setEngineDisabled(false);
-  };
-
-  const handleChangeEngine = (event) => {
-    setEngine(event.target.value);
-    setButtonDisabled(false);
+  const handleCalculateClick = () => {
+    history.push('/results');
   };
 
   return (
     <div>
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">Car Make</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={make}
-          onChange={handleChangeMake}
-          label="Car Make"
-        >
-          {databaseMakes.map((x) => (
-            <MenuItem value={x}>{x}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <Autocomplete
+        id="combo-box-demo"
+        options={locations}
+        getOptionLabel={(option) => `${option.town}, ${option.country} `}
+        style={{ width: 300 }}
+        onChange={handleChangeMake}
+        renderInput={(params) => (
+          <TextField {...params} label="Location" variant="outlined" />
+        )}
+      />
 
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">Model</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={model}
-          onChange={handleChangeModel}
-          label="Car Model"
-          disabled={modelDisabled}
-        >
-          {modelList.map((x) => (
-            <MenuItem value={x}>{x}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">Engine</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={engine}
-          onChange={handleChangeEngine}
-          label="Engine"
-          disabled={engineDisabled}
-        >
-          {engineList.map((x) => (
-            <MenuItem value={x}>{x}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Button variant="contained" disabled={buttonDisabled}>
+      <Button
+        variant="contained"
+        disabled={buttonDisabled}
+        onClick={handleCalculateClick}
+      >
         Calculate
       </Button>
     </div>
   );
 }
+
+const locations = [
+  { town: 'St Albans', country: 'England' },
+  { town: 'Harpenden', country: 'England' },
+];
 
 export default Form;
