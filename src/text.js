9f7c7d6c34b39e9248cdae34e5ea5dc9628eeaaf
J Perrin <perrinjack96@gmail.com>
2020-11-05 15:22:27 +0000

    Altered React class to react function

--- src/text.js
+++ src/text.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+
 function Test() {
   const counter = useSelector((state) => state.counter);
 
-  return <div>{counter}
-  </div>;
+  const endpointl =
+    'https://api.coronavirus.data.gov.uk/v1/data?' +
+    `filters=areaType=ltla;areaName=${counter}&` +
+    'structure={"date":"date","newCases":"newCasesByPublishDate"}';
+
+  const endpointn =
+    'https://api.coronavirus.data.gov.uk/v1/data?' +
+    'filters=areaType=nation;areaName=england&' +
+    'structure={"date":"date","newCases":"newCasesByPublishDate"}';
+
+  const localData = () => {
+    axios
+      .get(endpointl, { delay: 2000 })
+      .then((response) => {
+        console.log(response.data.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const nationwideData = () => {
+    axios
+      .get(endpointn, { delay: 2000 })
+      .then((response) => {
+        console.log(response.data.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    localData();
+    nationwideData();
+  });
+
+  return <div>{counter}</div>;
 }
 
 export default Test;
