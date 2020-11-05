5120ad5c81d7d4d2da1354c9a47a80d4c93341fa
J Perrin <perrinjack96@gmail.com>
2020-11-05 14:53:46 +0000

    Refactored the if statement

--- src/reducers/button.js
+++ src/reducers/button.js
@@ -1,11 +1,7 @@
 const buttonReducer = (state = true, action) => {
   switch (action.type) {
     case 'DARK':
-      if (action.payload === null) {
-        return true;
-      } else {
-        return false;
-      }
+      return !action.payload;
 
     default:
       return state;
