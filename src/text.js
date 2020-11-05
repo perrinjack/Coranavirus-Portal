8fdf1d2ecba89e340267f8570d9bb507d3fe0490
J Perrin <perrinjack96@gmail.com>
2020-11-05 17:14:44 +0000

    Local data mapped to list

--- src/text.js
+++ src/text.js
@@ -3,21 +3,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import { connect } from 'react-redux';
 
-const endpointl =
-  'https://api.coronavirus.data.gov.uk/v1/data?' +
-  `filters=areaType=ltla;areaName=&` +
-  'structure={"date":"date","newCases":"newCasesByPublishDate"}';
-
-const endpointn =
-  'https://api.coronavirus.data.gov.uk/v1/data?' +
-  'filters=areaType=nation;areaName=england&' +
-  'structure={"date":"date","newCases":"newCasesByPublishDate"}';
 class Test extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      localData: [],
+      nationalData: null,
+    };
+  }
   localData = () => {
     axios
-      .get(endpointl, { delay: 2000 })
+      .get(
+        'https://api.coronavirus.data.gov.uk/v1/data?' +
+          `filters=areaType=ltla;areaName=${this.props.count}&` +
+          'structure={"date":"date","newCases":"newCasesByPublishDate"}',
+        { delay: 2000 }
+      )
       .then((response) => {
-        console.log(response.data.data[0]);
+        console.log(response.data.data);
+        this.setState({
+          localData: response.data.data,
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -26,9 +32,14 @@ class Test extends React.Component {
 
   nationwideData = () => {
     axios
-      .get(endpointn, { delay: 2000 })
+      .get(
+        'https://api.coronavirus.data.gov.uk/v1/data?' +
+          'filters=areaType=nation;areaName=england&' +
+          'structure={"date":"date","newCases":"newCasesByPublishDate"}',
+        { delay: 2000 }
+      )
       .then((response) => {
-        console.log(response.data.data[0]);
+        this.setState({ nationalData: response.data.data[0].newCases });
       })
       .catch((error) => {
         console.log(error);
@@ -36,11 +47,20 @@ class Test extends React.Component {
   };
 
   componentDidMount() {
-    this.localData();
     this.nationwideData();
+    this.localData();
   }
   render() {
-    return <div>{this.props.count} </div>;
+    return (
+      <div>
+        {this.props.count} {this.state.nationalData}
+        {this.state.localData.slice(0, 7).map((num) => (
+          <p>
+            {num.date} : {num.newCases}
+          </p>
+        ))}
+      </div>
+    );
   }
 }
 
